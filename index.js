465ad5c7d1b5cfb3392386778643ef89facf8452
rad-corps <hulbert.adam@gmail.com>
2015-09-21 14:01:15 +1000

    player movement example

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/bin', express.static('bin'));
+app.use('/img', express.static('img'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  
+//receive and broadcast chat string
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log(msg);
+  });
+
+//receive and broadcast player pos
+  socket.on('player pos', function(pos_){
+  	io.emit('player pos', pos_);
+  	console.log('player pos: ' + pos_)
+  });
+});
+
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
