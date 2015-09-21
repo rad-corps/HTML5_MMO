f94108eee71a52adaad2d1655142e05e3a7a7d0e
rad-corps <hulbert.adam@gmail.com>
2015-09-21 16:49:59 +1000

    output disconnections to console

--- index.js
+++ index.js
@@ -12,22 +12,27 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   
-//receive and broadcast chat string
+  //receive and broadcast chat string
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log(msg);
   });
 
-//receive and broadcast player pos
+  //receive and broadcast player pos
   socket.on('player pos', function(pos_){
   	io.emit('player pos', pos_);
   	console.log('player pos: ' + pos_)
   });
+
+  //handle disconnection event
+  socket.on('disconnect', function() {
+    console.log('client disconnected');
+  });
+
+  console.log('new connection');
 });
 
-
-
-
+//start the webserver
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
